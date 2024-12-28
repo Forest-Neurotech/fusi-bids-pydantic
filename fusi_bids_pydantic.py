1ef860c63b91cdbe2ac667fdde10d98bfd94ee75
Charles Guan <3221512+charlesincharge@users.noreply.github.com>
2024-12-27 22:36:25 -0800

    Add single-file module

---
+++ fusi_bids_pydantic.py
@@ -0,0 +1,9 @@
+"""fUSI-BIDS Pydantic models for sidecar JSON schema."""
+
+from pydantic import BaseModel
+
+
+class FUSIBIDSModel(BaseModel):
+    """Base model for fUSI-BIDS sidecar JSON schema."""
+
+    pass
